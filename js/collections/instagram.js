8ec05233ed833657e532bd720375fd2da3c3b6c3
Matt Lindeboom <matt.lindeboom@gmail.com>
2013-07-19 18:02:44 -0400

    Adds working instagram call

---
+++ js/collections/instagram.js
@@ -0,0 +1,27 @@
+define([
+	'jquery',
+	'underscore',
+	'backbone'
+	], function($, _, Backbone){
+		var Instagram = Backbone.Collection.extend({
+
+			url: function () {
+				return 'https://api.instagram.com/v1/media/popular?client_id=' + this.client_id;
+			},
+			sync: function(method, model, options) {
+				var params = _.extend({
+					type: 'GET',
+					dataType: 'jsonp',
+					url: this.url(),
+					processData: false
+				}, options);
+				return $.ajax(params)
+			},
+			parse: function(resp, xhr) {
+					console.log(resp.data)
+					return resp.data;
+			},
+			client_id: '8fb211e761414d1eafe6eac2b917efd6'
+	});
+	return Instagram;
+});
--- js/views/users/UserView.js
+++ js/views/users/UserView.js
@@ -4,15 +4,30 @@ define([
 	'jquery',
 	'underscore',
 	'backbone',
+	'collections/instagram',
 	'text!templates/users/userTemplate.html'
-	], function($, _, Backbone, userTemplate){
-		var UserListView = Backbone.View.extend({
+	], function($, _, Backbone, InstagramCollection, userTemplate){
+		var UserView = Backbone.View.extend({
 			el: $('.content'),
+			initialize: function() {
+				this.isLoading = false;
+				this.instagramCollection = new InstagramCollection();
+			},
 			render: function(){
-				var data = {username :'Mary'};
-				var compiledTemplate = _.template(userTemplate, data);
-				this.$el.html(compiledTemplate);
+				this.loadResults();
+				//var compiledTemplate = _.template(userTemplate, data);
+				//this.$el.html(compiledTemplate);
+			},
+			loadResults: function(){
+				var that = this;
+				this.isLoading = true;
+				this.instagramCollection.fetch({
+					success: function(photos) {
+						console.log(photos.models)
+						$(that.el).append(_.template(userTemplate, {photos: photos.models, _:_}))
+					}
+				});
 			}
 		});
-		return UserListView;
+		return UserView;
 	});
